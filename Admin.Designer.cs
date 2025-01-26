e291d7ccb3d5d5d70ddf412c854e42ef41e732f8
JamesRussellDelaRosa <jamesdelosrosa@gmail.com>
2025-01-26 15:59:34 +0800

    added components in admin design GUI

--- Admin.Designer.cs
+++ Admin.Designer.cs
@@ -32,6 +32,10 @@
             search = new Button();
             delete = new Button();
             update = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             SuspendLayout();
             // 
             // add
@@ -74,11 +78,49 @@
             update.UseVisualStyleBackColor = true;
             update.Click += update_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(169, 151);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 28);
+            label1.TabIndex = 5;
+            label1.Text = "USERNAME";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(172, 207);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 28);
+            label2.TabIndex = 6;
+            label2.Text = "PASSWORD";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(322, 152);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(322, 211);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 8;
+            // 
             // Admin
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(update);
             Controls.Add(delete);
             Controls.Add(search);
@@ -86,6 +128,7 @@
             Name = "Admin";
             Text = "Admin";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -93,5 +136,9 @@
         private Button search;
         private Button delete;
         private Button update;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
