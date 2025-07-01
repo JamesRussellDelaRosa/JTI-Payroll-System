e1aabd59ab28bd4ea5effac23b153976afa7f15f
JamesRussellDelaRosa <jamesdelosrosa@gmail.com>
2025-07-01 10:20:06 +0800

    Add search functionality to employee records form
    Implemented a search button and text box in the `dgv_deducts_adds` form. Users can input a search term to highlight the first matching employee in the data grid view. A message box is shown if no matches are found.

--- dgv_deducts_adds.Designer.cs
+++ dgv_deducts_adds.Designer.cs
@@ -30,6 +30,8 @@
         {
             dataGridView1 = new DataGridView();
             SaveButton = new Button();
+            search = new Button();
+            searchtxt = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -52,17 +54,37 @@
             SaveButton.UseVisualStyleBackColor = true;
             SaveButton.Click += SaveButton_Click;
             // 
+            // search
+            // 
+            search.Location = new Point(1038, 10);
+            search.Name = "search";
+            search.Size = new Size(94, 29);
+            search.TabIndex = 72;
+            search.Text = "Search";
+            search.UseVisualStyleBackColor = true;
+            search.Click += search_Click;
+            // 
+            // searchtxt
+            // 
+            searchtxt.Location = new Point(837, 12);
+            searchtxt.Name = "searchtxt";
+            searchtxt.Size = new Size(195, 27);
+            searchtxt.TabIndex = 71;
+            // 
             // dgv_deducts_adds
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1137, 512);
+            Controls.Add(search);
+            Controls.Add(searchtxt);
             Controls.Add(SaveButton);
             Controls.Add(dataGridView1);
             Name = "dgv_deducts_adds";
             Text = "Deductions and Additionals";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
 
         }
 
@@ -70,5 +92,7 @@
 
         private DataGridView dataGridView1;
         private Button SaveButton;
+        private Button search;
+        private TextBox searchtxt;
     }
 }
